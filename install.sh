122c79f754ffe5e5c3c6e0330ca4ab2948540c94
Brian Han <sacahan@gmail.com>
2026-01-11 09:48:41 +0800

    Refactor code structure for improved readability and maintainability

--- install.sh
+++ install.sh
@@ -83,30 +83,39 @@ cat >"$WORKFLOW_PATH/Contents/Info.plist" <<'PLIST'
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-    <key>NSServices</key>
-    <array>
-        <dict>
-            <key>NSMenuItem</key>
-            <dict>
-                <key>default</key>
-                <string>移除浮水印</string>
-            </dict>
-            <key>NSMessage</key>
-            <string>runWorkflowAsService</string>
-            <key>NSRequiredContext</key>
-            <dict>
-                <key>NSApplicationIdentifier</key>
-                <string>com.apple.finder</string>
-            </dict>
-            <key>NSSendFileTypes</key>
-            <array>
-                <string>public.image</string>
-            </array>
-        </dict>
-    </array>
+	<key>NSServices</key>
+	<array>
+		<dict>
+			<key>NSBackgroundColorName</key>
+			<string>background</string>
+			<key>NSIconName</key>
+			<string>NSActionTemplate</string>
+			<key>NSMenuItem</key>
+			<dict>
+				<key>default</key>
+				<string>移除 Nano Banana Pro 浮水印</string>
+			</dict>
+			<key>NSMessage</key>
+			<string>runWorkflowAsService</string>
+			<key>NSRequiredContext</key>
+			<dict>
+				<key>NSApplicationIdentifier</key>
+				<string>com.apple.finder</string>
+			</dict>
+			<key>NSSendFileTypes</key>
+			<array>
+				<string>public.image</string>
+			</array>
+		</dict>
+	</array>
 </dict>
 </plist>
 PLIST
+
+# 產生固定 UUID（避免每次安裝都不同）
+INPUT_UUID="9802C4FA-8136-43E6-B590-9D11359AB39A"
+OUTPUT_UUID="C7241150-19BC-4FA9-BD30-8A418D686C45"
+ACTION_UUID="20AF5C3A-7C2A-4D90-92CD-8D2A22675E0F"
 
 # 建立 document.wflow
 cat >"$WORKFLOW_PATH/Contents/document.wflow" <<WFLOW
@@ -114,72 +123,64 @@ cat >"$WORKFLOW_PATH/Contents/document.wflow" <<WFLOW
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-    <key>AMApplicationBuild</key>
-    <string>523</string>
-    <key>AMApplicationVersion</key>
-    <string>2.10</string>
-    <key>AMDocumentVersion</key>
-    <string>2</string>
-    <key>actions</key>
-    <array>
-        <dict>
-            <key>action</key>
-            <dict>
-                <key>AMAccepts</key>
-                <dict>
-                    <key>Container</key>
-                    <string>List</string>
-                    <key>Optional</key>
-                    <true/>
-                    <key>Types</key>
-                    <array>
-                        <string>com.apple.cocoa.string</string>
-                    </array>
-                </dict>
-                <key>AMActionVersion</key>
-                <string>2.0.3</string>
-                <key>AMApplication</key>
-                <array>
-                    <string>Automator</string>
-                </array>
-                <key>AMCategory</key>
-                <string>AMCategoryUtilities</string>
-                <key>AMIconName</key>
-                <string>Run Shell Script</string>
-                <key>AMName</key>
-                <string>執行 Shell 工序指令</string>
-                <key>AMParameterProperties</key>
-                <dict>
-                    <key>COMMAND_STRING</key>
-                    <dict/>
-                    <key>CheckedForUserDefaultShell</key>
-                    <dict/>
-                    <key>inputMethod</key>
-                    <dict/>
-                    <key>shell</key>
-                    <dict/>
-                    <key>source</key>
-                    <dict/>
-                </dict>
-                <key>AMProvides</key>
-                <dict>
-                    <key>Container</key>
-                    <string>List</string>
-                    <key>Types</key>
-                    <array>
-                        <string>com.apple.cocoa.string</string>
-                    </array>
-                </dict>
-                <key>AMRequiredResources</key>
-                <array/>
-                <key>ActionBundlePath</key>
-                <string>/System/Library/Automator/Run Shell Script.action</string>
-                <key>ActionName</key>
-                <string>執行 Shell 工序指令</string>
-                <key>ActionParameters</key>
-                <dict>
-                    <key>COMMAND_STRING</key>
-                    <string>#!/bin/zsh
+	<key>AMApplicationBuild</key>
+	<string>528</string>
+	<key>AMApplicationVersion</key>
+	<string>2.10</string>
+	<key>AMDocumentVersion</key>
+	<string>2</string>
+	<key>actions</key>
+	<array>
+		<dict>
+			<key>action</key>
+			<dict>
+				<key>AMAccepts</key>
+				<dict>
+					<key>Container</key>
+					<string>List</string>
+					<key>Optional</key>
+					<true/>
+					<key>Types</key>
+					<array>
+						<string>com.apple.cocoa.string</string>
+					</array>
+				</dict>
+				<key>AMActionVersion</key>
+				<string>2.0.3</string>
+				<key>AMApplication</key>
+				<array>
+					<string>Automator</string>
+				</array>
+				<key>AMParameterProperties</key>
+				<dict>
+					<key>COMMAND_STRING</key>
+					<dict/>
+					<key>CheckedForUserDefaultShell</key>
+					<dict/>
+					<key>inputMethod</key>
+					<dict/>
+					<key>shell</key>
+					<dict/>
+					<key>source</key>
+					<dict/>
+				</dict>
+				<key>AMProvides</key>
+				<dict>
+					<key>Container</key>
+					<string>List</string>
+					<key>Types</key>
+					<array>
+						<string>com.apple.cocoa.string</string>
+					</array>
+				</dict>
+				<key>ActionBundlePath</key>
+				<string>/System/Library/Automator/Run Shell Script.action</string>
+				<key>ActionName</key>
+				<string>執行Shell工序指令</string>
+				<key>ActionParameters</key>
+				<dict>
+					<key>COMMAND_STRING</key>
+					<string>#!/bin/zsh
 
 # 設定 PATH 以確保找得到 python3
 export PATH="/usr/local/bin:/opt/homebrew/bin:/usr/bin:\$PATH"
@@ -193,7 +194,7 @@ FAIL=0
 
 # 處理每個輸入檔案
 for f in "\$@"; do
-    if /usr/bin/python3 "\$SCRIPT_PATH" "\$f" 2>/dev/null; then
+    if /usr/bin/python3 "\$SCRIPT_PATH" "\$f" 2&gt;/dev/null; then
         ((SUCCESS++))
     else
         ((FAIL++))
@@ -207,133 +208,168 @@ else
     osascript -e "display notification \"成功: \$SUCCESS, 失敗: \$FAIL\" with title \"KillWatermark\" sound name \"Basso\""
 fi
 </string>
-                    <key>CheckedForUserDefaultShell</key>
-                    <true/>
-                    <key>inputMethod</key>
-                    <integer>1</integer>
-                    <key>shell</key>
-                    <string>/bin/zsh</string>
-                    <key>source</key>
-                    <string></string>
-                </dict>
-                <key>BundleIdentifier</key>
-                <string>com.apple.RunShellScript</string>
-                <key>CFBundleVersion</key>
-                <string>2.0.3</string>
-                <key>CanShowSelectedItemsWhenRun</key>
-                <false/>
-                <key>CanShowWhenRun</key>
-                <true/>
-                <key>Category</key>
-                <array>
-                    <string>AMCategoryUtilities</string>
-                </array>
-                <key>Class Name</key>
-                <string>RunShellScriptAction</string>
-                <key>InputUUID</key>
-                <string>$(uuidgen)</string>
-                <key>Keywords</key>
-                <array>
-                    <string>Shell</string>
-                    <string>工序指令</string>
-                    <string>指令</string>
-                    <string>執行</string>
-                    <string>Unix</string>
-                </array>
-                <key>OutputUUID</key>
-                <string>$(uuidgen)</string>
-                <key>UUID</key>
-                <string>$(uuidgen)</string>
-                <key>UnlocalizedApplications</key>
-                <array>
-                    <string>Automator</string>
-                </array>
-                <key>arguments</key>
-                <dict>
-                    <key>0</key>
-                    <dict>
-                        <key>default value</key>
-                        <integer>0</integer>
-                        <key>name</key>
-                        <string>inputMethod</string>
-                        <key>required</key>
-                        <string>0</string>
-                        <key>type</key>
-                        <string>0</string>
-                        <key>uuid</key>
-                        <string>0</string>
-                    </dict>
-                    <key>1</key>
-                    <dict>
-                        <key>default value</key>
-                        <string></string>
-                        <key>name</key>
-                        <string>source</string>
-                        <key>required</key>
-                        <string>0</string>
-                        <key>type</key>
-                        <string>0</string>
-                        <key>uuid</key>
-                        <string>1</string>
-                    </dict>
-                    <key>2</key>
-                    <dict>
-                        <key>default value</key>
-                        <false/>
-                        <key>name</key>
-                        <string>CheckedForUserDefaultShell</string>
-                        <key>required</key>
-                        <string>0</string>
-                        <key>type</key>
-                        <string>0</string>
-                        <key>uuid</key>
-                        <string>2</string>
-                    </dict>
-                    <key>3</key>
-                    <dict>
-                        <key>default value</key>
-                        <string></string>
-                        <key>name</key>
-                        <string>COMMAND_STRING</string>
-                        <key>required</key>
-                        <string>0</string>
-                        <key>type</key>
-                        <string>0</string>
-                        <key>uuid</key>
-                        <string>3</string>
-                    </dict>
-                    <key>4</key>
-                    <dict>
-                        <key>default value</key>
-                        <string>/bin/sh</string>
-                        <key>name</key>
-                        <string>shell</string>
-                        <key>required</key>
-                        <string>0</string>
-                        <key>type</key>
-                        <string>0</string>
-                        <key>uuid</key>
-                        <string>4</string>
-                    </dict>
-                </dict>
-                <key>isViewVisible</key>
-                <integer>1</integer>
-                <key>location</key>
-                <string>529.000000:253.000000</string>
-                <key>nibPath</key>
-                <string>/System/Library/Automator/Run Shell Script.action/Contents/Resources/Base.lproj/main.nib</string>
-            </dict>
-            <key>isViewVisible</key>
-            <integer>1</integer>
-        </dict>
-    </array>
-    <key>connectors</key>
-    <dict/>
-    <key>workflowMetaData</key>
-    <dict>
-        <key>workflowTypeIdentifier</key>
-        <string>com.apple.Automator.servicesMenu</string>
-    </dict>
+					<key>CheckedForUserDefaultShell</key>
+					<true/>
+					<key>inputMethod</key>
+					<integer>1</integer>
+					<key>shell</key>
+					<string>/bin/zsh</string>
+					<key>source</key>
+					<string></string>
+				</dict>
+				<key>BundleIdentifier</key>
+				<string>com.apple.RunShellScript</string>
+				<key>CFBundleVersion</key>
+				<string>2.0.3</string>
+				<key>CanShowSelectedItemsWhenRun</key>
+				<false/>
+				<key>CanShowWhenRun</key>
+				<true/>
+				<key>Category</key>
+				<array>
+					<string>AMCategoryUtilities</string>
+				</array>
+				<key>Class Name</key>
+				<string>RunShellScriptAction</string>
+				<key>InputUUID</key>
+				<string>${INPUT_UUID}</string>
+				<key>Keywords</key>
+				<array>
+					<string>Shell</string>
+					<string>工序指令</string>
+					<string>指令</string>
+					<string>執行</string>
+					<string>Unix</string>
+				</array>
+				<key>OutputUUID</key>
+				<string>${OUTPUT_UUID}</string>
+				<key>UUID</key>
+				<string>${ACTION_UUID}</string>
+				<key>UnlocalizedApplications</key>
+				<array>
+					<string>Automator</string>
+				</array>
+				<key>arguments</key>
+				<dict>
+					<key>0</key>
+					<dict>
+						<key>default value</key>
+						<integer>0</integer>
+						<key>name</key>
+						<string>inputMethod</string>
+						<key>required</key>
+						<string>0</string>
+						<key>type</key>
+						<string>0</string>
+						<key>uuid</key>
+						<string>0</string>
+					</dict>
+					<key>1</key>
+					<dict>
+						<key>default value</key>
+						<false/>
+						<key>name</key>
+						<string>CheckedForUserDefaultShell</string>
+						<key>required</key>
+						<string>0</string>
+						<key>type</key>
+						<string>0</string>
+						<key>uuid</key>
+						<string>1</string>
+					</dict>
+					<key>2</key>
+					<dict>
+						<key>default value</key>
+						<string></string>
+						<key>name</key>
+						<string>source</string>
+						<key>required</key>
+						<string>0</string>
+						<key>type</key>
+						<string>0</string>
+						<key>uuid</key>
+						<string>2</string>
+					</dict>
+					<key>3</key>
+					<dict>
+						<key>default value</key>
+						<string></string>
+						<key>name</key>
+						<string>COMMAND_STRING</string>
+						<key>required</key>
+						<string>0</string>
+						<key>type</key>
+						<string>0</string>
+						<key>uuid</key>
+						<string>3</string>
+					</dict>
+					<key>4</key>
+					<dict>
+						<key>default value</key>
+						<string>/bin/sh</string>
+						<key>name</key>
+						<string>shell</string>
+						<key>required</key>
+						<string>0</string>
+						<key>type</key>
+						<string>0</string>
+						<key>uuid</key>
+						<string>4</string>
+					</dict>
+				</dict>
+				<key>isViewVisible</key>
+				<integer>1</integer>
+				<key>location</key>
+				<string>309.000000:305.000000</string>
+				<key>nibPath</key>
+				<string>/System/Library/Automator/Run Shell Script.action/Contents/Resources/Base.lproj/main.nib</string>
+			</dict>
+			<key>isViewVisible</key>
+			<integer>1</integer>
+		</dict>
+	</array>
+	<key>connectors</key>
+	<dict/>
+	<key>workflowMetaData</key>
+	<dict>
+		<key>applicationBundleID</key>
+		<string>com.apple.finder</string>
+		<key>applicationBundleIDsByPath</key>
+		<dict>
+			<key>/System/Library/CoreServices/Finder.app</key>
+			<string>com.apple.finder</string>
+		</dict>
+		<key>applicationPath</key>
+		<string>/System/Library/CoreServices/Finder.app</string>
+		<key>applicationPaths</key>
+		<array>
+			<string>/System/Library/CoreServices/Finder.app</string>
+		</array>
+		<key>inputTypeIdentifier</key>
+		<string>com.apple.Automator.fileSystemObject.image</string>
+		<key>outputTypeIdentifier</key>
+		<string>com.apple.Automator.nothing</string>
+		<key>presentationMode</key>
+		<integer>15</integer>
+		<key>processesInput</key>
+		<false/>
+		<key>serviceApplicationBundleID</key>
+		<string>com.apple.finder</string>
+		<key>serviceApplicationPath</key>
+		<string>/System/Library/CoreServices/Finder.app</string>
+		<key>serviceInputTypeIdentifier</key>
+		<string>com.apple.Automator.fileSystemObject.image</string>
+		<key>serviceOutputTypeIdentifier</key>
+		<string>com.apple.Automator.nothing</string>
+		<key>serviceProcessesInput</key>
+		<false/>
+		<key>systemImageName</key>
+		<string>NSActionTemplate</string>
+		<key>useAutomaticInputType</key>
+		<false/>
+		<key>workflowTypeIdentifier</key>
+		<string>com.apple.Automator.servicesMenu</string>
+	</dict>
 </dict>
 </plist>
 WFLOW
